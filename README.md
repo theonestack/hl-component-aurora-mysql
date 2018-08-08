c4c5b3b3b514ae686d845d9401a1112f92d43ea1
guslington <viney-34@hotmail.com>
2018-08-08 14:41:38 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+![build-status](https://travis-ci.com/theonestack/hl-component-ecs.svg?branch=master)
+
+### Cfhighlander aurora 2 mysql component
+
+**Engine:** MySQL 5.7
+
+```bash
+
+# install highlander gem
+$ gem install cfhighlander
+
+# build and validate standalone component
+$ cfhighlander cfcompile --validate ecs
+
+```
+
+
+### Parameters
+
+TBD
+
+### Configuration options
+
+TBD
+
+### Outputs
+
+TBD
---
+++ aurora2-mysql.cfhighlander.rb
@@ -0,0 +1,25 @@
+CfhighlanderTemplate do
+  DependsOn 'vpc@1.2.0'
+  Parameters do
+    ComponentParam 'EnvironmentName', 'dev', isGlobal: true
+    ComponentParam 'EnvironmentType', 'development', isGlobal: true
+    MappingParam('WriterInstanceType') do
+      map 'EnvironmentType'
+      attribute 'WriterInstanceType'
+    end
+    MappingParam('ReaderInstanceType') do
+      map 'EnvironmentType'
+      attribute 'ReaderInstanceType'
+    end
+    MappingParam('DnsDomain') do
+      map 'AccountId'
+      attribute 'DnsDomain'
+    end
+    maximum_availability_zones.times do |az|
+      ComponentParam "SubnetData#{az}"
+    end
+    ComponentParam 'SnapshotID'
+    ComponentParam 'EnableReader', 'false'
+    ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
+  end
+end
