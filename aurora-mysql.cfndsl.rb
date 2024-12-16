176b8741c7702039616a468dcef1a10c6b851e32
petlitskiy <81190543+petlitskiy@users.noreply.github.com>
2024-12-16 23:52:31 +0200

    Update aurora-mysql.cfndsl.rb
    Use of external_parameters[:dns_format]

--- aurora-mysql.cfndsl.rb
+++ aurora-mysql.cfndsl.rb
@@ -168,8 +168,8 @@ CloudFormation do
 
     Route53_RecordSet(:DBClusterReaderRecord) {
       Condition(:EnableReader)
-      HostedZoneName FnJoin('', [ Ref(:EnvironmentName), '.', Ref(:DnsDomain), '.' ])
-      Name FnJoin('', [ external_parameters[:hostname_read_endpoint], '.', Ref(:EnvironmentName), '.', Ref(:DnsDomain), '.' ])
+      HostedZoneName FnSub("#{external_parameters[:dns_format]}.")
+      Name FnSub("#{external_parameters[:hostname_read_endpoint]}.#{external_parameters[:dns_format]}.")
       Type 'CNAME'
       TTL '60'
       ResourceRecords [ FnGetAtt('DBCluster','ReadEndpoint.Address') ]
@@ -177,8 +177,8 @@ CloudFormation do
   end
 
   Route53_RecordSet(:DBHostRecord) {
-    HostedZoneName FnJoin('', [ Ref(:EnvironmentName), '.', Ref(:DnsDomain), '.' ])
-    Name FnJoin('', [ external_parameters[:hostname], '.', Ref(:EnvironmentName), '.', Ref(:DnsDomain), '.' ])
+    HostedZoneName FnSub("#{external_parameters[:dns_format]}.")
+    Name FnSub("#{external_parameters[:hostname]}.#{external_parameters[:dns_format]}.")
     Type 'CNAME'
     TTL '60'
     ResourceRecords [ FnGetAtt('DBCluster','Endpoint.Address') ]
