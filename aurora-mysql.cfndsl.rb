2e0d4e427829028b9ba286a40967581e9ac57dc9
petlitskiy <81190543+petlitskiy@users.noreply.github.com>
2025-01-19 17:21:22 +0200

    Removed comments

--- aurora-mysql.cfndsl.rb
+++ aurora-mysql.cfndsl.rb
@@ -277,8 +277,8 @@ CloudFormation do
   ApplicationAutoScaling_ScalableTarget(:ServiceScalingTarget) do
     DependsOn 'RDSReplicaAutoScaleRole'
     Condition 'EnableReplicaAutoScaling'
-    MaxCapacity FnJoin('', ['0', Ref(:ScalableTargetMaxCapacity)]) # Ref makes a String. The lead zero does the trick: CloudFormation parses it as a Number.
-    MinCapacity FnJoin('', ['0', Ref(:ScalableTargetMinCapacity)]) # Ref makes a String. The lead zero does the trick: CloudFormation parses it as a Number.
+    MaxCapacity FnJoin('', ['0', Ref(:ScalableTargetMaxCapacity)])
+    MinCapacity FnJoin('', ['0', Ref(:ScalableTargetMinCapacity)])
     ResourceId FnJoin(':',["cluster",Ref(:DBCluster)])
     RoleARN FnGetAtt(:RDSReplicaAutoScaleRole,:Arn)
     ScalableDimension "rds:cluster:ReadReplicaCount"
